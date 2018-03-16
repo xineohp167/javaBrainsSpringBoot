091fc545d561d4e7bd95563b2f096ddec3253eca
fabien <fabien.kunis@gmail.com>
2018-03-16 09:35:39 +0200

    Adding demo2 with spring data jpa

---
+++ javaBrainsDemo2Data/src/main/java/io/javabrains/springbootstarter/JavaBrainsDemo2DataApplication.java
@@ -0,0 +1,12 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBrainsDemo2DataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBrainsDemo2DataApplication.class, args);
+	}
+}
