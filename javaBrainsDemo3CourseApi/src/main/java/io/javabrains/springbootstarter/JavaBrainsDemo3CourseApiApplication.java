721629f9d43acb41995dd9f521845f944bc5a75f
fabien <fabien.kunis@gmail.com>
2018-03-16 20:49:37 +0200

    Adding demo 3 course api

---
+++ javaBrainsDemo3CourseApi/src/main/java/io/javabrains/springbootstarter/JavaBrainsDemo3CourseApiApplication.java
@@ -0,0 +1,12 @@
+package io.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBrainsDemo3CourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBrainsDemo3CourseApiApplication.class, args);
+	}
+}
---
+++ javaBrainsDemo3CourseApi/src/main/java/io/javabrains/springbootstarter/course/CourseController.java
@@ -0,0 +1,49 @@
+package io.javabrains.springbootstarter.course;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.springbootstarter.topic.Topic;
+
+@RestController
+public class CourseController {
+	
+	@Autowired
+	private CourseService courseService;
+	
+	@RequestMapping("/topics/{id}/courses")
+	public List<Course> getAllTopics(@PathVariable String id){
+		return courseService.getAllCourses(id);
+	}
+	
+	@RequestMapping("/topics/{topicId}/courses/{id}")
+	public Course getTopic(@PathVariable String id) {
+		return courseService.getCourse(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics/{topicsId}/courses")
+	public void addTopic(@RequestBody Course course, @PathVariable String topicsId) {
+		course.setTopic(new Topic(topicsId, "", ""));
+		courseService.addCourse(course);
+	}
+	
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{topicsId}/courses/{id}")
+	public void updateTopic(@RequestBody Course course, @PathVariable String topicsId, @PathVariable String id) {
+		course.setTopic(new Topic(topicsId, "", ""));
+		courseService.updateCourse(course);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{topicsId}/courses/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		courseService.deleteCourse(id);
+	}
+
+
+}
